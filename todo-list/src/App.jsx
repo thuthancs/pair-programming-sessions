8413203628763a117041c871ea1656524e01df90
Thu Than <than@uni.minerva.edu>
2025-10-30 15:38:55 -0700

    fix: updated handleSubmit function to return an object and change shift() to unshift() to insert the item to the front of the array

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -2,21 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  // Initialize the todo list which will be updated via the function setTodo
-  const [todoItem, setTodoItem] = useState("")
-  const [todoList, setTodoList] = useState([todoItem])
+  // Initialize a todo item as an empty object and a todo list as an empty array
+  const [todoItem, setTodoItem] = useState({})
+  const [todoList, setTodoList] = useState([])
 
   // Whenever there is a change in the input of todo item, change the item based on the input
   function handleChange(e) {
-    setTodoItem(e.target.value)
+    setTodoItem({
+      id: crypto.randomUUID(),
+      name: e.target.value,
+      done: false
+    })
     console.log(todoItem)
   }
+
+  function handleComplete(e) {
+    if (e.target.value == "checked") {
+      setTodoItem({
+        ...todoItem,
+        done: true
+      })
+    }
+  }
   
-  // Update the todo list
+  // Update the todo list by adding the recently added item to the top of the array
   const handleSubmit = () => {
-    const [todoItem, ...todoList] = todoList
-    todoList.shift(todoItem)
+    todoList.unshift(todoItem)
     setTodoList(todoList)
+    
     console.log(todoList)
   }
 
@@ -25,11 +38,16 @@ function App() {
     <>
       <h1>Todo List</h1>
       <div>
-        <input onChange={handleChange}></input>
+        <input value={todoItem.name} onChange={handleChange}></input>
         <button onClick={handleSubmit}>Submit</button>
       </div>
       <div>
-        {todoList}
+        {todoList.map(todoItem => (
+          <div onChange={handleComplete} key={todoItem.id}>
+            <input type="checkbox"></input>
+            {todoItem.name}
+          </div>
+        ))}
       </div>
     </>
   )
